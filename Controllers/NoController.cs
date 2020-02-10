f2e433d4577d18638bd8514c3e697eb7996017d9
Sydney Kovach <sk072215@ohio.edu>
2020-02-10 17:02:16 -0500

    no more controller plz

--- Controllers/NoController.cs
+++ Controllers/NoController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MvcMovie2.Controllers
+/*namespace MvcMovie2.Controllers
 {
     public class HomeController : Controller
     {
